f8be9c3d37ce08c0a8844fbd3f9db0e207a4b304
Karl Fritsche <github@attrib.org>
2022-09-19 13:59:23 +0200

    Deployment to attrib.org

--- apps/client/src/environments/environment.prod.ts
+++ apps/client/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   verboseOperations: false,
   firebase: {
-    projectId: 'lostark-helper-8dfb0',
-    appId: '1:149724175977:web:07f4bb26bb508e98a18cf8',
-    storageBucket: 'lostark-helper-8dfb0.appspot.com',
-    apiKey: 'AIzaSyAS587wsXY3WqaWchRGdQ66IQrVk2kjEyE',
-    authDomain: 'lostark-helper-8dfb0.firebaseapp.com',
-    messagingSenderId: '149724175977',
+    apiKey: "AIzaSyCj6hstWC8QwAJQHLaGrdy5yY9XQ4mWaXQ",
+    authDomain: "lostarkhelper-363011.firebaseapp.com",
+    projectId: "lostarkhelper-363011",
+    storageBucket: "lostarkhelper-363011.appspot.com",
+    messagingSenderId: "222627140382",
+    appId: "1:222627140382:web:a31df0efa5082736676f0f"
   },
   production: true,
   marketApi: 'https://www.lostarkmarket.online/api'
